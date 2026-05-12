ffc71a7b7b6b00b1e630680673bf887d317e01b6
Jeemit12 <jeemitpatel12072006@gmail.com>
2026-05-12 23:31:48 +0530

    Done_Day15

---
+++ Day15/Minimum_Initial_Energy_to_Finish_Tasks.cpp
@@ -0,0 +1,32 @@
+/*
+You are given an array tasks where tasks[i] = [actuali, minimumi]:
+
+actuali is the actual amount of energy you spend to finish the ith task.
+minimumi is the minimum amount of energy you require to begin the ith task.
+For example, if the task is [10, 12] and your current energy is 11, you cannot start this task. However, if your current energy is 13, you can complete this task, and your energy will be 3 after finishing it.
+
+You can finish the tasks in any order you like.
+
+Return the minimum initial amount of energy you will need to finish all the tasks.
+*/
+class Solution {
+public:
+    int minimumEffort(vector<vector<int>>& tasks) {
+        sort(tasks.begin(), tasks.end(),
+            [](vector<int>& a, vector<int>& b) {
+                return (a[1] - a[0]) > (b[1] - b[0]);
+        });
+        int ans=0;
+        int ene=0;
+        for(auto &t : tasks){
+            int min=t[1];
+            int req=t[0];
+            if(ene<min){
+                ans+=(min-ene);
+                ene=min;
+            }
+            ene-=req;
+        }
+        return ans;
+    }
+};
