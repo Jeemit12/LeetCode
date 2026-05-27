7cf202bc3e8368d1f4a950d4ad00c8a7fcaf6ba7
Jeemit12 <jeemitpatel12072006@gmail.com>
2026-05-27 23:25:38 +0530

    Done_Day30

---
+++ Day30/Count_the_Number_of_Special_Characters_II.cpp
@@ -0,0 +1,34 @@
+/*
+You are given a string word. A letter c is called special if it appears both in lowercase and uppercase in word, and every lowercase occurrence of c appears before the first uppercase occurrence of c.
+
+Return the number of special letters in word.
+*/
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        vector<int> lastLower(26, -1);
+        vector<int> firstUpper(26, 1e9);
+
+        for (int i = 0; i < word.size(); i++) {
+            char ch = word[i];
+
+            if (islower(ch)) {
+                lastLower[ch - 'a'] = i;
+            } else {
+                firstUpper[ch - 'A'] = min(firstUpper[ch - 'A'], i);
+            }
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if (lastLower[i] != -1 &&
+                firstUpper[i] != 1e9 &&
+                lastLower[i] < firstUpper[i]) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+};
