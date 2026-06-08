7f66c180850571a0c64ee5155f6c6182658a2609
Jeemit12 <jeemitpatel12072006@gmail.com>
2026-06-08 23:09:13 +0530

    Done_Day42

---
+++ Day42/Partition_Array_According_to_Given_Pivot.cpp
@@ -0,0 +1,31 @@
+/*
+You are given a 0-indexed integer array nums and an integer pivot. Rearrange nums such that the following conditions are satisfied:
+
+Every element less than pivot appears before every element greater than pivot.
+Every element equal to pivot appears in between the elements less than and greater than pivot.
+The relative order of the elements less than pivot and the elements greater than pivot is maintained.
+More formally, consider every pi, pj where pi is the new position of the ith element and pj is the new position of the jth element. If i < j and both elements are smaller (or larger) than pivot, then pi < pj.
+Return nums after the rearrangement.
+*/
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int> less,equal,larger;
+        for(int x:nums){
+            if(x<pivot){
+                less.push_back(x);
+            }
+            else if(x==pivot){
+                equal.push_back(x);
+            }
+            else{
+                larger.push_back(x);
+            }
+        }
+        vector<int> ans;
+        ans.insert(ans.end(),less.begin(),less.end());
+        ans.insert(ans.end(),equal.begin(),equal.end());
+        ans.insert(ans.end(),larger.begin(),larger.end());
+        return ans;
+    }
+};
