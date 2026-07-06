953e7092d82c5d8ac2381d51010d1de599021207
Jeemit12 <jeemitpatel12072006@gmail.com>
2026-07-06 22:01:31 +0530

    Done_Day70

---
+++ Day70/Remove_Covered_Intervals.cpp
@@ -0,0 +1,30 @@
+/*
+Given an array intervals where intervals[i] = [li, ri] represent the interval [li, ri), remove all intervals that are covered by another interval in the list.
+
+The interval [a, b) is covered by the interval [c, d) if and only if c <= a and b <= d.
+
+Return the number of remaining intervals.
+*/
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(),
+             [](const vector<int>& a, const vector<int>& b) {
+                 if (a[0] == b[0])
+                     return a[1] > b[1];   
+                 return a[0] < b[0];       
+             });
+
+        int ans = 0;
+        int maxEnd = -1;
+
+        for (auto &it : intervals) {
+            if (it[1] > maxEnd) {
+                ans++;
+                maxEnd = it[1];
+            }
+        }
+
+        return ans;
+    }
+};
