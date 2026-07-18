c85799dc4a42562cc107a76184cabe5c5c857693
Jeemit12 <jeemitpatel12072006@gmail.com>
2026-07-18 23:11:41 +0530

    Done_Day82

---
+++ Day82/Find_Greatest_Common_Divisor_of_Array.cpp
@@ -0,0 +1,13 @@
+/*
+Given an integer array nums, return the greatest common divisor of the smallest number and largest number in nums.
+
+The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
+*/
+class Solution {
+public:
+    int findGCD(vector<int>& nums) {
+        int mx = *max_element(nums.begin(), nums.end());
+        int mn = *min_element(nums.begin(), nums.end());
+        return gcd(mx, mn);
+    }
+};
