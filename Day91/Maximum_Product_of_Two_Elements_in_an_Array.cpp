36552eb8583ed815eb80782c77ef9ea73fd5771a
Jeemit12 <jeemitpatel12072006@gmail.com>
2026-07-27 22:20:05 +0530

    Done_Day91

---
+++ Day91/Maximum_Product_of_Two_Elements_in_an_Array.cpp
@@ -0,0 +1,19 @@
+/*
+Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
+*/
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int m1=0,m2=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>m1){
+                m2=m1;
+                m1=nums[i];
+            }
+            else if(nums[i]>m2){
+                m2=nums[i];
+            }
+        }
+        return (m1-1)*(m2-1);
+    }
+};
