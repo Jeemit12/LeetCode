1adcf56503dedd13f010cc733d52be756056c33f
Jeemit12 <jeemitpatel12072006@gmail.com>
2026-09-16 21:10:26 +0530

    Done_Day142

---
+++ Day142/Number_of_Sets_of_K_Non-Overlapping_Line_Segments.cpp
@@ -0,0 +1,27 @@
+/*
+Given n points on a 1-D plane, where the ith point (from 0 to n-1) is at x = i, find the number of ways we can draw exactly k non-overlapping line segments such that each segment covers two or more points. The endpoints of each segment must have integral coordinates. The k line segments do not have to cover all n points, and they are allowed to share endpoints.
+
+Return the number of ways we can draw k non-overlapping line segments. Since this number can be huge, return it modulo 109 + 7.
+*/
+const int MOD = 1000000007;
+
+class Solution {
+public:
+    int numberOfSets(int n, int k) {
+        vector<int> dp(n), prefixSums(n + 1);
+        for (int j = 0; j < n; j++) {
+            dp[j] = 1;
+            prefixSums[j + 1] = (prefixSums[j] + dp[j]) % MOD;
+        }
+        for (int i = 1; i <= k; i++) {
+            dp[0] = 0;
+            for (int j = 1; j < n; j++) {
+                dp[j] = (dp[j - 1] + prefixSums[j]) % MOD;
+            }
+            for (int j = 0; j < n; j++) {
+                prefixSums[j + 1] = (prefixSums[j] + dp[j]) % MOD;
+            }
+        }
+        return dp[n - 1];
+    }
+};
